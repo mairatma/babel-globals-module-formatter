487f218568210b0068519d06c8749757380ee1fc
Maira Bello <maira.araujo@liferay.com>
2015-04-09 14:52:33 -0300

    Replaces imports with assignments from global variables

--- index.js
+++ index.js
@@ -1,28 +1,68 @@
-var babel = require('babel')
+var babel = require('babel');
+var path = require('path');
 
-function GlobalsFormatter() {}
+var t = babel.types;
+
+function GlobalsFormatter(file) {
+  if (file.opts.filename === 'unknown') {
+    throw new Error('The babel globals module formatter requires that filename be given');
+  }
+
+  this._filename = file.opts.filename;
+  this._globalName = file.opts._globalName || 'es6Globals';
+  this._sourceRoot = file.opts.sourceRoot || process.cwd();
+}
 
 GlobalsFormatter.prototype.transform = function (ast) {
   // this is ran after all transformers have had their turn at modifying the ast
   // feel free to modify this however
 };
 
+/**
+ * Handles imports that don't store the results in variables. For example:
+ * `import 'foo'`.
+ * @param  {!ImportDeclaration} node
+ * @param  {!Array} nodes
+ */
 GlobalsFormatter.prototype.importDeclaration = function (node, nodes) {
-  // node is an ImportDeclaration
+  // Just ignore this import, since the dependency code will already have been run.
 };
 
 GlobalsFormatter.prototype.importSpecifier = function (specifier, node, nodes) {
-  // specifier is an ImportSpecifier
-  // node is an ImportDeclaration
+  if (t.isSpecifierDefault(specifier)) {
+    // import foo from "foo";
+    var id = this.getGlobalIdentifier(node.source.value);
+    nodes.push(t.variableDeclaration('var', [
+      t.variableDeclarator(specifier.local, id)
+    ]));
+  } else if (t.isImportNamespaceSpecifier(specifier)) {
+    // import * as bar from "foo";
+    console.log('wildcard import');
+  } else {
+    // import { foo } from "foo";
+    console.log('named import');
+  }
 };
 
 GlobalsFormatter.prototype.exportDeclaration = function (node, nodes) {
-  // node is an ExportDeclaration
 };
 
 GlobalsFormatter.prototype.exportSpecifier = function (specifier, node, nodes) {
-  // specifier is an ExportSpecifier
-  // node is an ExportDeclaration
+};
+
+GlobalsFormatter.prototype.getGlobalIdentifier = function(filePath, name, isWildcard) {
+  var globalName = this._globalName;
+  if (name || isWildcard) {
+    globalName += 'Named';
+  }
+
+  filePath = path.resolve(path.dirname(this._filename), filePath);
+  var splitPath = filePath.split(path.sep);
+  var moduleName = splitPath[splitPath.length - 1];
+
+  var id = 'this.' + globalName + '.' + moduleName + (name ? '.' + name : '');
+
+  return t.identifier(id);
 };
 
 module.exports = GlobalsFormatter;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,47 @@
 var assert = require('assert');
+var babel = require('babel');
+var fs = require('fs');
 var GlobalsFormatter = require('../index');
+var path = require('path');
 
 module.exports = {
-	testEmpty: function() {
-		assert.ok(false, 'TODO');
+	testNoFilename: function(test) {
+		assert.throws(function() {
+			babel.transform('var a = 2;', getBabelOptions());
+		});
+		test.done();
+	},
+
+	testGlobalIdentifier: function(test) {
+		var babelOptions = getBabelOptions(path.resolve('foo/bar.js'));
+		var formatter = new GlobalsFormatter({opts: babelOptions});
+
+		assert.strictEqual('this.myGlobal.foo', formatter.getGlobalIdentifier('./foo').name);
+		assert.strictEqual('this.myGlobal.bar', formatter.getGlobalIdentifier('./bar').name);
+		assert.strictEqual('this.myGlobal.foo', formatter.getGlobalIdentifier('./../foo').name);
+		assert.strictEqual('this.myGlobalNamed.foo.bar', formatter.getGlobalIdentifier('./../foo', 'bar').name);
+		assert.strictEqual('this.myGlobalNamed.foo', formatter.getGlobalIdentifier('./../foo', null, true).name);
+
+		test.done();
+	},
+
+	testDefaultImport: function(test) {
+		var code = 'import foo from "./foo"';
+		var babelOptions = getBabelOptions(path.resolve('foo/bar.js'));
+		var result = babel.transform(code, babelOptions);
+
+		var expectedResult = 'use strict;\n\nvar foo = this.myGlobal.foo;';
+		assert.notStrictEqual(expectedResult, result);
+
+		test.done();
 	}
 };
+
+function getBabelOptions(filename, sourceRoot) {
+	return {
+		_globalName: 'myGlobal',
+		filename: filename,
+		modules: GlobalsFormatter,
+		sourceRoot: sourceRoot
+	};
+}
